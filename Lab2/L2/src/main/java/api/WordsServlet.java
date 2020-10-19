12044a995bdce6afb4a0b787129ef1b1e4895abb
unknown <Andrei.Mihaila@centric.eu>
2020-10-20 02:46:12 +0300

    get a;;

--- Lab2/L2/src/main/java/api/WordsServlet.java
+++ Lab2/L2/src/main/java/api/WordsServlet.java
@@ -33,4 +33,10 @@ public class WordsServlet extends HttpServlet {
             req.getRequestDispatcher("/error.jsp").forward(req,resp);
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("result", repo.GetAllWords());
+        req.getRequestDispatcher("/result.jsp").forward(req,resp);
+    }
 }
